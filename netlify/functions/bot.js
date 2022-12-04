527d8ffc53ddf6846229c1c566fb49694a0e21cb
larchanka <horrorgr@gmail.com>
2022-12-04 11:50:53 +0100

    Delete message sending

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,23 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Replace this with your Telegram bot token
-const token = process.env.BOT_API;
-
-// Create a new Telegram bot instance
-const bot = new TelegramBot(token);
-
-// Register a 'message' event handler
-bot.on('message', async msg => {
-  console.log('New message', msg);
-  // Check if the message is a system message
-  if (msg.new_chat_members || msg.left_chat_member || msg.group_chat_created || msg.supergroup_chat_created || msg.channel_chat_created || msg.migrate_to_chat_id || msg.migrate_from_chat_id || msg.pinned_message) {
-    // Delete the message if it's a system message
-    bot.deleteMessage(msg.chat.id, msg.message_id);
-  }
-});
-
 exports.handler = async (event, context, callback) => {
-  console.log('New request');
+  // Replace this with your Telegram bot token
+  const token = process.env.BOT_API;
+  
+  // Create a new Telegram bot instance
+  const bot = new TelegramBot(token);
+  
+  // Register a 'message' event handler
+  bot.on('message', async msg => {
+    console.log('New message', msg);
+    // Check if the message is a system message
+    if (msg.new_chat_members || msg.left_chat_member || msg.group_chat_created || msg.supergroup_chat_created || msg.channel_chat_created || msg.migrate_to_chat_id || msg.migrate_from_chat_id || msg.pinned_message) {
+      // Delete the message if it's a system message
+      bot.deleteMessage(msg.chat.id, msg.message_id);
+    }
+  });
+
   // Parse the request body
   const { body } = event;
 
@@ -35,10 +34,7 @@ exports.handler = async (event, context, callback) => {
       body: 'OK'
     };
 
-    // Encode the response as a JSON string
-    const responseBody = JSON.stringify(response);
-
     // Return the response to the bot
-    callback(null, responseBody);
+    callback(null, JSON.stringify(response));
   }
 };
