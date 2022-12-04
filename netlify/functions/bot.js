ce619d7896037d7dc05bab316e3f3fd5d03c25ec
larchanka <horrorgr@gmail.com>
2022-12-04 03:10:05 +0100

    Init

---
+++ netlify/functions/bot.js
@@ -0,0 +1,45 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Replace this with your Telegram bot token
+const token = process.env.BOT_API;
+
+// Create a new Telegram bot instance
+const bot = new TelegramBot(token);
+
+// Register a 'message' event handler
+bot.on('message', async msg => {
+  // Check if the message is a system message
+  if (
+    msg.new_chat_members || 
+    msg.left_chat_member || 
+    msg.group_chat_created || 
+    msg.supergroup_chat_created || 
+    msg.channel_chat_created || 
+    msg.migrate_to_chat_id || 
+    msg.migrate_from_chat_id || 
+    msg.pinned_message
+  ) {
+    // Delete the message if it's a system message
+    bot.deleteMessage(msg.chat.id, msg.message_id);
+  }
+});
+
+exports.handler = async (event, context) => {
+  // Parse the request body
+  const { body } = event;
+
+  // Check if the request is a webhook update
+  if (body) {
+    // Parse the update from the request body
+    const update = JSON.parse(body);
+
+    // Process the update using the bot
+    await bot.processUpdate(update);
+
+    // Return a 200 response to acknowledge receipt of the update
+    return {
+      statusCode: 200,
+      body: 'OK'
+    };
+  }
+};
